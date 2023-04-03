7f31bb5be916b1c680504920866e1943c60a0c36
MARIA EDUARDA MORENO DOS SANTOS <maria.2960@aluno.pr.senac.br>
2023-04-03 11:52:21 -0300

    Atividade

---
+++ seventh.dart
@@ -0,0 +1,17 @@
+void main(){
+  String Linguagem = 'PHP';
+  switch (Linguagem){
+    case "HTML":
+      print("super daora");
+      break;
+    case "PHP":
+      print("uma bosta");
+      break;
+    case "JavaScript":
+      print("a mais pica");
+      break;
+    default:
+      print("não conheço essa linguagem");
+      break;
+  }
+}
---
+++ sixth.dart
@@ -0,0 +1,7 @@
+void main() {
+
+  int idade = 18;
+  String msg = idade >= 18 ? "Maior de idade" : "Menor de idade";
+
+  print(msg);
+}
