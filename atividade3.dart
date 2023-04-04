6d8634bdab4f9271fc6858df7ad1592298411462
MARIA EDUARDA MORENO DOS SANTOS <maria.2960@aluno.pr.senac.br>
2023-04-04 16:37:49 -0300

    Atividade Tabuada

---
+++ atividade3.dart
@@ -0,0 +1,7 @@
+void main() {
+  int numero = 2;
+  for (int i= 1; i <= 10; i++){
+  int resultado = numero * i;
+  print(resultado);
+  }
+}
---
+++ nineth.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<String> frutas = ['maça', 'banana', 'laranja'];
+  for (String frutas in frutas) {
+    print(frutas);
+  }
+}
